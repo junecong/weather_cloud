6deef7d39151b46a9877b58baaf281309f7c5f51
June Cong <junzhucong@gmail.com>
2016-10-13 22:21:46 -0700

    initial commit

---
+++ weather_cloud.py
@@ -0,0 +1,41 @@
+"""Script to get the weather and change my Lifex bulb to tell it to me in the morning."""
+import sys
+
+"""import schedule to schedule lights everyday at 7am-9am"""
+# import schedule
+# import time
+
+# def job():
+#     print("I'm working...")
+
+
+# schedule.every().day.at("7:00").do(job)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+
+
+
+
+
+
+URL = "http://www.google.com"
+DEGREE = 65
+
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def get_weather_for_the_day(url):
+	"""Return what the weather will be like for the day"""
+	pass
+
+def calculate_color_based_on_weather(degree):
+    """Return a list of keywords associated with *movie*."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
