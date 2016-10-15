4a6f527d2fcb5ec607555d9e7d22a8cb969bb48f
June Cong <junzhucong@gmail.com>
2016-10-15 13:17:25 -0700

    refactor to change weather apis to darksky due to rain limitations with wunderground

--- weather_cloud.py
+++ weather_cloud.py
@@ -1,60 +1,65 @@
-#Script to get the weather and change my Lifex bulb to tell it to me in the morning.
+#Hello, this is a script to get the weather at my house and and change my Lifex bulb to tell it to me in the morning.
 
+#import schedule to schedule lights everyday at 7am-9am
 import sys
-import schedule #import schedule to schedule lights everyday at 7am-9am
+import schedule 
+#import for lifx lightbulb settings
 import time
-import requests #import for lifx lightbulb settings
+import requests 
+#import for weather json parsing
 import json
-import urllib2 #import for weather json parsing
+import urllib2 
 
-color = "white" #color I want the bulb to change to
-degree = 0 #what the weather feels like at the moment, not actual temperature
-rain = False #will it rain today?
-rainfall = 0
-
-
-
+token = "YOUR TOKEN HERE" 
 headers = {
     "Authorization": "Bearer %s" % token,
 }
 
+rain = False
+degree = 0
+
 def get_weather_for_the_day():
-	f = urllib2.urlopen('http://api.wunderground.com/api/7d7772c918e39295/conditions/q/CA/San_Francisco.json')
+	f = urllib2.urlopen('URL FOR WEATHER CALL HERE')
 	json_string = f.read()
 	parsed_json = json.loads(json_string)
-	degree = parsed_json['current_observation']['feelslike_f']
+	degree = parsed_json['currently']['apparentTemperature']
 	degree = float(degree)
-	rainfall = parsed_json['current_observation']['precip_today_string']
-	rainfall = float(rainfall[0:4])
+	rainfall = parsed_json['daily']['data'][0]['precipProbability']
+	rainfall = float(rainfall)
 	if rainfall > 0:
 		rain = True
 	print "Current temperature is: %s" % (degree)
-	print "Will it rain? %s" % (rainfall)
+	print "Will it rain? %s" % (rain)
 	f.close()
 	return degree
 
-def color_based_on_weather(degree):
-    if degree >= 80:
-    	print "Red and %s" % (degree)
-        color = 'red'
-    elif degree >= 70:
-    	print "orange and %s" % (degree)
-        color = 'orange'
-    elif degree >= 63:
-    	print "white and %s" % (degree)
-        color = 'white'
-    elif degree >= 55:
-    	print "blue and %s" % (degree)
-        color = 'blue'
-    else:
-    	print "purple and %s" % (degree)
-        color = 'purple'
-    return color
+def color_based_on_weather(degree, rain):
+	if rain:
+		print "Pink and %s" % (degree)
+		color = 'pink'
+	else:
+	    if degree >= 80:
+	    	print "Red and %s" % (degree)
+	        color = 'red'
+	    elif degree >= 70:
+	    	print "orange and %s" % (degree)
+	        color = 'orange'
+	    elif degree >= 63:
+	    	print "white and %s" % (degree)
+	        color = 'white'
+	    elif degree >= 55:
+	    	print "blue and %s" % (degree)
+	        color = 'blue'
+	    else:
+	    	print "purple and %s" % (degree)
+	        color = 'purple'
+	    return color
+	return color
 
 def on():
 	print("Let there be light")
 	degree = get_weather_for_the_day()
-	color = color_based_on_weather(degree)
+	color = color_based_on_weather(degree, rain)
 
 	#Lastly, you want to set the color and pass it to the bulb
 	payload = {
@@ -65,29 +70,22 @@ def on():
 	}
 	response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
 	
-	# if rain == True:
-	# 	data = {
-	#     "period": 2,
-	#     "color": "pink",
-	# 	}
-	# 	response = requests.post('https://api.lifx.com/v1/lights/all/effects/breathe', data=data, headers=headers)
-
 def off():
 	payload = {
 	    "power": "off",
 	}
 
-# #Set the bulb to do this every day at 7:00am
-# schedule.every().day.at("0:25").do(on)
-# #Set the bulb to turn off every day at 9:00am
-# schedule.every().day.at("9:00").do(off)
 
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
+#Set the bulb to do this every day at 7:00am
+schedule.every().day.at("13:20").do(on)
+#Set the bulb to turn off every day at 9:00am
+schedule.every().day.at("13:22").do(off)
 
 while True:
-    on()
+    schedule.run_pending()
+    time.sleep(1)
+
+# Test module
+# while True:
+# on()
 
